30a7c42ed7b325a95a5b46df820accf9fc1e6962
Soeren Berg <soeren.berg@protonmail.com>
2024-12-01 16:26:49 +0100

    Year 2024 Day01 part B

--- haskell/src/Year2024/Day01.hs
+++ haskell/src/Year2024/Day01.hs
@@ -9,10 +9,15 @@ totalDistance :: [(Int, Int)] -> Int
 totalDistance = sum . (map dist) . (uncurry zip) . (over both sort) . unzip
   where dist (a,b) = abs (a-b)
 
+similarityScore :: [(Int, Int)] -> Int
+similarityScore = sum . getScores . unzip
+  where getScores (xs, ys) = map (\x -> x * x `countIn` ys) xs
+        countIn x = length . filter (==x)
+
 line :: Parser (Int, Int)
 line = (,) <$> digits <* whitespace <*> digits <* eol
 
 solve :: IO (Int, Int)
 solve = do input <- pack <$> readFile "data/Year2024/day01.txt"
            let ps = fromRight [] $ parse (many line) "" input
-           return (totalDistance ps, 0)
+           return (totalDistance ps, similarityScore ps)
