889a438471f431a4c68a82876a96583f7b1e84cc
Soeren Berg <soeren.berg@protonmail.com>
2024-12-04 19:20:12 +0100

    Year 2024 Day04 part B

--- haskell/src/Year2024/Day04.hs
+++ haskell/src/Year2024/Day04.hs
@@ -10,8 +10,13 @@ countXMASat g k _ n = (+n) . sum . map (\ds -> isXMASinDir k ds g) $ dirs
 isXMASinDir :: Z2 -> [Z2] -> Grid Char -> Int
 isXMASinDir k ds g' = fromEnum . (=="XMAS") . lookupSeqAt k ds $ g'
 
+isMASat :: Grid Char -> Z2 -> Char -> Int -> Int
+isMASat g k _ n = (+n) . fromEnum . (`S.member` ys) . takeEvens . neighbors9At g $ k
+ where ys = S.fromList ["MMASS", "SMASM", "SSAMM", "MSAMS"]
+       takeEvens xs = [x | (i,x) <- zip [0..] xs, even i]
+
 solve :: IO (Int, Int)
 solve = do g <- fromLines id . lines <$> readFile "data/Year2024/day04.txt"
            let partA = M.foldrWithKey (countXMASat g) 0 g
-           let partB = 0
+           let partB = M.foldrWithKey (isMASat g) 0 g
            return (partA, partB)
