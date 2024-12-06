bcddad28c719b3f7ee6b6525523d6402ae25559f
Soeren Berg <soeren.berg@protonmail.com>
2024-12-06 11:25:16 +0100

    Slight cleanup

--- haskell/src/Year2024/Day05.hs
+++ haskell/src/Year2024/Day05.hs
@@ -4,34 +4,31 @@ import qualified Data.Set as S
 
 import AdventOfCode
 
-lookupFromRules :: [(Int,Int)] -> M.Map Int (S.Set Int)
-lookupFromRules = foldr update M.empty
- where update (x,y) t = M.insertWith S.union x (S.singleton y) t
-
-makeCmp :: M.Map Int (S.Set Int) -> Int -> Int -> Ordering
-makeCmp t x y = let s = M.findWithDefault S.empty x t in
-                if y `S.member` s then LT else GT
+makeCmp :: S.Set (Int, Int) -> Int -> Int -> Ordering
+makeCmp t x y = if (x,y) `S.member` t then LT else GT
 
 mid :: [a] -> a
 mid xs = xs !! (length xs `div` 2)
 
 partA :: (Int -> Int -> Ordering) -> [[Int]] -> Int
-partA c = sum . map mid . filter (isOrdered c)
+partA c = foldr f 0
+  where f a b = b + if isOrdered c a then mid a else 0
 
 isOrdered :: (a -> a -> Ordering) -> [a] -> Bool
 isOrdered c = all (==LT) . (zipWith c <*> tail)
 
 partB :: (Int -> Int -> Ordering) -> [[Int]] -> Int
-partB c = sum . map (mid . sortBy c) . filter (not . isOrdered c)
+partB c = foldr f 0
+  where f a b = b + if isOrdered c a then 0 else mid . sortBy c $ a
 
-file :: Parser ([(Int,Int)], [[Int]])
+file :: Parser (S.Set (Int,Int), [[Int]])
 file = do rs <- many $ (,) <$> digits <* char '|' <*> digits <* eol
           _ <- eol
           us <- many $ digits `sepBy` char ',' <* eol
-          return (rs, us)
+          return (S.fromList rs, us)
 
 solve :: IO (Int, Int)
 solve = do input <- pack <$> readFile "data/Year2024/day05.txt"
-           let (rs, us) = fromRight ([],[]) $ parse file "" input
-           let cmp = makeCmp . lookupFromRules $ rs
+           let (rs, us) = fromRight (S.empty,[]) $ parse file "" input
+           let cmp = makeCmp rs
            return (partA cmp us, partB cmp us)
