b9d6b27e9ffbb87196723d7743e8af6a4aa36195
Soeren Berg <soeren.berg@protonmail.com>
2024-12-07 09:21:37 +0100

    Year 2024 Day07 part B

--- haskell/src/Year2024/Day07.hs
+++ haskell/src/Year2024/Day07.hs
@@ -1,7 +1,4 @@
 module Year2024.Day07 where
-import qualified Data.Map as M
-import qualified Data.Set as S
-
 import AdventOfCode
 
 
@@ -16,6 +13,9 @@ cal :: [Int -> Int -> Int] -> [(Int, [Int])] -> Int
 cal ops = foldr (\(r, xs) n -> n + (f $ check ops r (head xs) (tail xs))) 0
   where f xs = if null xs then 0 else head xs
 
+(##) :: Int -> Int -> Int
+(##) a b = read $ (show a) ++ (show b)
+
 line :: Parser (Int, [Int])
 line = do r <- digits <* char ':' <* whitespace
           xs <- digits `sepBy` whitespace <* eol
@@ -24,4 +24,4 @@ line = do r <- digits <* char ':' <* whitespace
 solve :: IO (Int, Int)
 solve = do input <- pack <$> readFile "data/Year2024/day07.txt"
            let ps = fromRight [] $ parse (many line) "" input
-           return (cal [(+),(*)] ps, 0)
+           return (cal [(+),(*)] ps, cal [(+),(*),(##)] ps)
