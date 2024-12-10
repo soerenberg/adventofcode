941e64e49b1c41a8a02fd67ed9949b1832a4020d
Soeren Berg <soeren.berg@protonmail.com>
2024-12-10 22:48:27 +0100

    Year 2024 day 09 part B

--- haskell/src/Year2024/Day09.hs
+++ haskell/src/Year2024/Day09.hs
@@ -24,8 +24,37 @@ moveSingles ls = moveSingles' (length zs) ls zs
 checkSum :: [Int] -> Int
 checkSum = sum . zipWith (*) [0..]
 
+data Block = B Int Int | BSpc Int deriving (Eq, Show)
+
+toBlocks :: [Int] -> [Block]
+toBlocks = toBlocks' True . (zip [0..])
+  where toBlocks' _ [] = []
+        toBlocks' True ((i,x):xs) = (B x (i `div` 2)):(toBlocks' False xs)
+        toBlocks' False ((_,x):xs) = (BSpc x):(toBlocks' True xs)
+
+moveBlocks :: [Block] -> [Block]
+moveBlocks xs = foldr moveBlock xs xs
+
+moveBlock :: Block -> [Block] -> [Block]
+moveBlock (BSpc _) xs = xs
+moveBlock b@(B l i) (bb@(B l' i'):xs) = if b == bb then (B l i):xs
+                                        else (B l' i'):(moveBlock (B l i) xs)
+moveBlock b@(B l i) ((BSpc ls):xs)
+  | l == ls = (B l i):xs
+  | l < ls = (B l i):(BSpc (ls-l)):xs
+  | otherwise = (BSpc ls):(moveBlock b xs)
+
+fromBlocks :: [Block] -> [Int]
+fromBlocks = fromBlocks' S.empty
+  where fromBlocks' :: S.Set Int -> [Block] -> [Int]
+        fromBlocks' _ [] = []
+        fromBlocks' s ((BSpc l):xs) = (replicate l 0) ++ fromBlocks' s xs
+        fromBlocks' s ((B l i):xs) =
+          if i `S.member` s then (replicate l 0) ++ fromBlocks' s xs
+          else (replicate l i) ++ (fromBlocks' (S.insert i s) xs)
 
 solve :: IO (Int, Int)
 solve = do input <- map digitToInt . init <$> readFile "data/Year2024/day09.txt"
            return $ over both checkSum (
-             moveSingles . toSingles $ input, [])
+             moveSingles . toSingles $ input,
+             fromBlocks . moveBlocks . toBlocks $ input)
