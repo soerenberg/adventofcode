bad6a56b602724f378e6f9cbf62e6a7ab19fa7be
Soeren Berg <soeren.berg@protonmail.com>
2025-01-03 18:04:05 +0100

    Use library Dijkstra in Day18

--- haskell/src/Year2024/Day18.hs
+++ haskell/src/Year2024/Day18.hs
@@ -1,35 +1,28 @@
 module Year2024.Day18 where
 import qualified Data.List as L
 import qualified Data.Map as M
-import qualified Data.Set as S
 import AdventOfCode
-
-dijkstra :: (a -> Bool) -> Grid a -> Z2 -> M.Map Z2 Int
-dijkstra p g s = dijkstra' (M.fromList ((s,0):initL)) (S.singleton s) p g
-  where initL = map (_2 .~ 1) . filter (p . snd) . neighbors4AtList g $ s
-
-dijkstra' :: M.Map Z2 Int -> S.Set Z2 -> (a -> Bool) -> Grid a -> M.Map Z2 Int
-dijkstra' acc vis p g =
-  let us = M.toList . M.filterWithKey (\k _ -> not . (`S.member` vis) $ k) $ acc
-      (minZ,minD) = minimumBy (\(_,a) (_,b) -> compare a b) $ us
-      validZs = map fst . filter (p . snd) . neighbors4AtList g $ minZ
-      newAcc = foldr (\z m -> M.insertWith min z (minD+1) m) acc validZs in
-  if L.null us
-  then acc
-  else dijkstra' newAcc (S.insert minZ vis) p g
 
 partA :: Int -> Int -> [Z2] -> Maybe Int
 partA l n xs = let
   zs = [0..l]
   g = M.fromList [((i,j),True) | i<-[0..l], j<-[0..l]]
-  gg = foldr (\(i,j) b -> M.insert (j,i) False b) g . take n $ xs in
-  M.lookup (l,l) . dijkstra id gg $ (0,0)
+  gg = foldr (\(i,j) -> M.insert (j,i) False) g . take n $ xs in
+  liftM fst . M.lookup (l,l) . dijkstra (0,0) $ (toAdj gg)
 
-partB :: Int -> Int -> [Z2] -> Maybe Int
-partB _ 0 _ = Nothing
-partB l n xs = maybe (partB l (n-1) xs) (Just . const (n+1)) (partA l n xs)
+toAdj :: M.Map Z2 Bool -> (Z2 -> [(Z2, Int)])
+toAdj g = map (_2 .~ 1) . filter snd . neighbors4AtList g
 
-solve :: IO (Maybe Int, Maybe Int)
+partB :: Int -> Int -> [Z2] -> Maybe String
+partB l n xs = do
+  i <- getIndex l n xs
+  return . L.intercalate "," . (^.. each) . over both show . (!! i) $ xs
+  where getIndex _ 0 _ = Nothing
+        getIndex l' n' xs' = case partA l' n' xs' of
+                            Nothing -> getIndex l' (n'-1) xs'
+                            (Just _) -> Just n'
+
+solve :: IO (Maybe Int, Maybe String)
 solve = do input <- pack <$> readFile "data/Year2024/day18.txt"
            let xs = fromRight [] $ parse (many line2Digits) "" input
            return (partA 70 1024 xs, partB 70 (length xs) xs)
