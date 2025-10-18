1f032f20a07b6f348527fbb3866bb5f823e88033
Soeren Berg <soeren.berg@protonmail.com>
2025-10-19 00:24:09 +0200

    Checks for year2022 day02

--- haskell/test/Tests/Year2022/Day02.hs
+++ haskell/test/Tests/Year2022/Day02.hs
@@ -11,8 +11,9 @@ import Year2022.Day02 (solve)
 
 
 exampleInput :: String
-exampleInput = """
-"""
+exampleInput = """A Y
+B X
+C Z"""
 
 data_input :: String
 data_input = unsafePerformIO . readFile $ inputFilePath 2022 2
@@ -22,14 +23,14 @@ tests = [exampleChecks, solutionChecks]
 
 exampleChecks :: TestTree
 exampleChecks = testGroup "exampleChecks"
-    [ testCase "exampleA" $ (fst <$> solution) @?= (Right 0)
-    , testCase "exampleB" $ (snd <$> solution) @?= (Right 0)
+    [ testCase "exampleA" $ (fst <$> solution) @?= (Right 15)
+    , testCase "exampleB" $ (snd <$> solution) @?= (Right 12)
     ]
   where solution = solve exampleInput
 
 solutionChecks :: TestTree
 solutionChecks = testGroup "solutionChecks"
-    [ testCase "A" $ (fst <$> solution) @?= (Right 0)
-    , testCase "B" $ (snd <$> solution) @?= (Right 0)
+    [ testCase "A" $ (fst <$> solution) @?= (Right 12156)
+    , testCase "B" $ (snd <$> solution) @?= (Right 10835)
     ]
   where solution = solve data_input
