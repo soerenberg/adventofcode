3fac46a28c72c64e0d6886dd1fe734fb0c238a80
Soeren Berg <soeren.berg@protonmail.com>
2025-10-19 00:26:04 +0200

    Checks for year2022 day03

--- haskell/test/Tests/Year2022/Day03.hs
+++ haskell/test/Tests/Year2022/Day03.hs
@@ -11,8 +11,12 @@ import Year2022.Day03 (solve)
 
 
 exampleInput :: String
-exampleInput = """
-"""
+exampleInput = """vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"""
 
 data_input :: String
 data_input = unsafePerformIO . readFile $ inputFilePath 2022 3
@@ -22,14 +26,14 @@ tests = [exampleChecks, solutionChecks]
 
 exampleChecks :: TestTree
 exampleChecks = testGroup "exampleChecks"
-    [ testCase "exampleA" $ (fst <$> solution) @?= (Right 0)
-    , testCase "exampleB" $ (snd <$> solution) @?= (Right 0)
+    [ testCase "exampleA" $ (fst <$> solution) @?= (Right 157)
+    , testCase "exampleB" $ (snd <$> solution) @?= (Right 70)
     ]
   where solution = solve exampleInput
 
 solutionChecks :: TestTree
 solutionChecks = testGroup "solutionChecks"
-    [ testCase "A" $ (fst <$> solution) @?= (Right 0)
-    , testCase "B" $ (snd <$> solution) @?= (Right 0)
+    [ testCase "A" $ (fst <$> solution) @?= (Right 7889)
+    , testCase "B" $ (snd <$> solution) @?= (Right 2825)
     ]
   where solution = solve data_input
