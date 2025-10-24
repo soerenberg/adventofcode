46a3a92799ef2f3babcc2a1f9a58f71bf4f87709
Soeren Berg <soeren.berg@protonmail.com>
2025-10-24 23:53:07 +0200

    year 2016 day 07 part a refactor

--- haskell/src/Year2016/Day07.hs
+++ haskell/src/Year2016/Day07.hs
@@ -8,12 +8,6 @@ import AdventOfCode
 
 import Debug.Trace (trace)
 
-exampleInput :: String
-exampleInput = """abba[mnop]qrst
-abcd[bddb]xyyx
-aaaa[qwer]tyui
-ioxxoj[asdfgh]zxcvbn"""
-
 isAbba :: (Char, Char, Char, Char) -> Bool
 isAbba (a,b,c,d) = a == d && b == c && a /= b
 
@@ -21,23 +15,20 @@ hasAbba :: String -> Bool
 hasAbba (a:b:c:d:xs) = isAbba (a,b,c,d) || hasAbba (b:c:d:xs)
 hasAbba _ = False
 
-hasTLS :: ([String], [String]) -> Bool
-hasTLS (ls,rs) = any hasAbba ls && (not . any hasAbba $ rs)
+hasTLS :: [String] -> Bool
+hasTLS xs = hasTLS' True False xs
+  where hasTLS' True b (x:xs) = hasTLS' False (b || hasAbba x) xs
+        hasTLS' False b (x:xs) = (not . hasAbba $ x) && hasTLS' True b xs
+        hasTLS' _ b [] = b
 
-splitAltRev :: [a] -> ([a], [a])
-splitAltRev xs = splitAlt' xs False ([], [])
-  where splitAlt' [] _ t = t
-        splitAlt' (x:ys) False (l,r) = splitAlt' ys True (x:l, r)
-        splitAlt' (x:ys) True (l,r) = splitAlt' ys False (l, x:r)
-
-line :: Parser ([String], [String])
+line :: Parser [String]
 line = do
   xs <- letters `sepBy1` (char '[' <|> char ']')
   _ <- eolf
-  pure . splitAltRev $ xs
+  pure xs
 
 solve :: String -> Either ParseError (Int, Int)
 solve t = do
     ls <- parse (many line) "" (pack t)
     let n = length . filter hasTLS $ ls
-    pure (n,2)
+    pure (n, 0)
